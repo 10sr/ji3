6b8772bddd4a80b315e3176dbd062109fe2f1bbb
10sr <8slashes+git@gmail.com>
2011-12-19 13:38:59 +0900

    compile error alu.v

--- t/alu.v
+++ t/alu.v
@@ -1,1 +1,35 @@
-module alu();
+module alu(op, tr, sr, clk, dr, cf, of);
+   input [3:0] op;
+   input [31:0] tr;
+   input [31:0] sr;
+   input        clk;
+   
+
+   output [31:0] dr;
+   output        cf;
+   output        of;
+
+   reg [31:0]    dr;
+   reg           cf;
+
+   assign of = 0;
+   
+   // wire [7:0]    sim8;
+   
+   // assign sim8 <= sim[7:0];
+   
+   case (op)
+     4'b0000: dr <= tr + sr;      // add
+     4'b0001: dr <= tr - sr;      // sub
+     4'b0010: dr <= tr == sr;              // cmp
+     4'b0011: dr <= tr & sr;      // and
+     4'b0100: dr <= tr | sr;      // or
+     4'b0101: dr <= tr ^ sr;      // xor
+     4'b0110: dr <= ! tr + 1;     // neg
+     4'b0111: dr <= ! tr;         // not
+     4'b1000: {cf, dr} <= {1'b0, tr} << (sr & 31'o0037);  // sll
+     4'b1001: {cf, dr} <= {1'b0, tr} << (sr & 31'o0037);  // sla
+     4'b1010: {dr, cf} <= {tr, 1'b0} >> (sr & 31'o0037);  // srl
+     4'b1011: {dr, cf} <= {tr, 1'b0} >>> (sr & 31'o0037); // sra
+   endcase // case (op)
+endmodule // alu
--- t/testalu.v
+++ t/testalu.v
@@ -1,21 +1,32 @@
 module testalu;
 
-   reg [3:0] op, [31:0] tr, [31:0] sr, [31:0] dr, cf;
+   reg [3:0] op;
+   reg [31:0] tr;
+   reg [31:0] sr;
+   // reg        clk;
+   reg [31:0] dr;
+   reg        cf;
    
    alu a(op, tr, sr, dr, cf);
+
+   // initial begin
+   //    clk <= 0;
+   //    forever #5 clk = ~clk;
+   // end
+   
    
    initial begin
-      #10 op <= 3'b0000; tr <= 32; sr <= 21;
-      #10 op <= 3'b0001;
-      #10 op <= 3'b0010;
-      #10 op <= 3'b0011;
-      #10 op <= 3'b0100;
-      #10 op <= 3'b0101;
-      #10 op <= 3'b0110;
-      #10 op <= 3'b0111;
-      #10 op <= 3'b1000; sr <= 3;
-      #10 op <= 3'b1001;
-      #10 op <= 3'b1010;
-      #10 op <= 3'b1011;
+      #10 op <= 4'b0000; tr <= 32; sr <= 21;
+      #10 op <= 4'b0001;
+      #10 op <= 4'b0010;
+      #10 op <= 4'b0011;
+      #10 op <= 4'b0100;
+      #10 op <= 4'b0101;
+      #10 op <= 4'b0110;
+      #10 op <= 4'b0111;
+      #10 op <= 4'b1000; sr <= 3;
+      #10 op <= 4'b1001;
+      #10 op <= 4'b1010;
+      #10 op <= 4'b1011;
    end // initial begin
 endmodule // testalu
